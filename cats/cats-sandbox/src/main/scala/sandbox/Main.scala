7a74d235d1b331f9ddb815fbf75770da0d80faf8
John Wei <john.wei@rea-group.com>
2019-06-12 08:37:08 +1000

    Scala-With-Cats C1 Eq Option

--- cats/cats-sandbox/src/main/scala/sandbox/Main.scala
+++ cats/cats-sandbox/src/main/scala/sandbox/Main.scala
@@ -1,8 +1,17 @@
 package sandbox
 
-import c1.show._
-import cats.syntax.show._
+// import cats.Eq
+import cats.instances.int._
+import cats.instances.string._
+import cats.instances.option._
+import cats.syntax.eq._
+import cats.syntax.option._
 
 object Main extends App {
-  println(Cat("Tom", 12, "Brown").show)
+  println(s"Test: ${123 === 123}")
+  println(s"Test: ${"123" =!= "1243"}")
+  println(s"Test: ${ (Some(1): Option[Int]) === (None: Option[Int]) }")
+  println(s"Test: ${ Option(1) === Option.empty[Int]}")
+  println(s"Test: ${ 1.some === 1.some}")
+  println(s"Test: ${ 1.some =!= none[Int]}")
 }
