ccaf24030bd318bbce12696b773164c8dd196d0a
John Wei <john.wei@rea-group.com>
2019-06-16 17:20:51 +1000

    Scala-With-Cats C4 Monad Eval stack safety

---
+++ cats/cats-sandbox/TODO.d
@@ -0,0 +1,8 @@
+
+- 3.6 Contravariant and Invariant Functors
+  Come back here before reading Chapter 6.
+- 4.5 Aside: Error Handling and MonadError
+  Come back here if it is needed when reading Chapter 5.
+  We won’t need to use MonadError unless we need to abstract over error handling monads.
+  For example, you can use MonadError to ab- stract over Future and Try,
+  or over Either and EitherT (which we will meet in Chapter 5).
--- cats/cats-sandbox/src/main/scala/sandbox/Main.scala
+++ cats/cats-sandbox/src/main/scala/sandbox/Main.scala
@@ -1,20 +1,8 @@
 package sandbox
 
-import c3.tree._
-import cats.Functor
-import cats.syntax.show._
-import cats.syntax.functor._
+import c4.eval._
 
 object Main extends App {
-  val functorTree = Functor[Tree]
-  val branch = Branch(Leaf(1), Leaf(2))
-  val branch1 = functorTree.map(Leaf(2))(_ * 2)
-  val tree = Tree.branch(Tree.leaf(1), Tree.leaf(2))
-  println(s"tree: ${tree.show}")
-  val tree1 = functorTree.map(tree)(_ * 2)
-  println(s"tree1: ${tree1.show}")
-  val tree2 = Tree.leaf(1).map(_ * 2)
-  println(s"tree2: ${tree2.show}")
-  val tree3 = Tree.branch(Tree.leaf(1), Tree.leaf(2)).map(_ * 2)
-  println(s"tree3: ${tree3.show}")
+  val result = SaferFolding.foldRight((1 to 100000).toList, 0L)(_ + _)
+  println(s"Result: $result")
 }
