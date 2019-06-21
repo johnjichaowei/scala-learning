75c81b692af1f00bec2f0853b177a3257b17e0d8
John Wei <john.wei@rea-group.com>
2019-06-21 17:54:42 +1000

    Scala-With-Cats C4 Monad State post order calculator exercise

--- cats/cats-sandbox/src/main/scala/sandbox/Main.scala
+++ cats/cats-sandbox/src/main/scala/sandbox/Main.scala
@@ -6,4 +6,16 @@ object Main extends App {
   val (state, result) = StateDemo.demo.run(1).value
 
   println(s"State demo result: state - $state, result - $result")
+
+  val (state1, result1) = PostOrderCalculator.evalOne("42").run(Nil).value
+  println(s"evalOne1 result: ($state1, $result1)")
+
+  val demoEvalOne = PostOrderCalculator.demoEvalOne.run(Nil).value
+  println(s"demoEvalOne: $demoEvalOne")
+
+  val demoEvalAll = PostOrderCalculator.demoEvalAll.run(Nil).value
+  println(s"demoEvalAll: $demoEvalAll")
+
+  val calcResult = PostOrderCalculator.evalInput("1 3 + 5 4 + * 1 3 + *")
+  println(s"calcResult: $calcResult")
 }
--- cats/cats-sandbox/src/main/scala/sandbox/c4/state.scala
+++ cats/cats-sandbox/src/main/scala/sandbox/c4/state.scala
@@ -1,6 +1,7 @@
 package sandbox.c4.state
 
 import cats.data.State
+import cats.syntax.applicative._
 
 object StateDemo {
   val demo: State[Int, (Int, Int, Int)] = for {
@@ -10,4 +11,52 @@ object StateDemo {
     _ <- State.modify[Int](_ + 1)
     c <- State.inspect[Int, Int](_ * 1000)
   } yield (a, b, c)
+}
+
+object PostOrderCalculator {
+  type CalcState[A] = State[List[Int], A]
+
+  def evalInput(input: String): Int =
+    evalAll(input.split(" ").toList).runA(Nil).value
+
+  def evalAll(input: List[String]): CalcState[Int] =
+    input.foldLeft(0.pure[CalcState]) { (a, b) =>
+      a.flatMap(_ => evalOne(b))
+    }
+
+  def evalOne(sym: String): CalcState[Int] = sym match {
+    case "+" => operator(_ + _)
+    case "-" => operator(_ - _)
+    case "*" => operator(_ * _)
+    case "/" => operator(_ / _)
+    case num => operand(num.toInt)
+  }
+
+  def operand(num: Int): CalcState[Int] =
+    State[List[Int], Int] { stack =>
+      (num :: stack, num)
+    }
+
+  def operator(func: (Int, Int) => Int): CalcState[Int] =
+    State[List[Int], Int] {
+      case b :: a :: tail =>
+        val ans = func(a, b)
+        (ans :: tail, ans)
+      case _ =>
+        sys.error("Fail!")
+    }
+
+  val demoEvalOne = for {
+    _ <- evalOne("12")
+    _ <- evalOne("24")
+    _ <- evalOne("+")
+    _ <- evalOne("3")
+    ans <- evalOne("*")
+  } yield ans
+
+  val demoEvalAll = for {
+    _ <- evalAll(List("12", "20", "+", "12", "-"))
+    _ <- evalAll(List("20", "2", "/", "3", "*"))
+    ans <- evalOne("+")
+  } yield ans
 }
